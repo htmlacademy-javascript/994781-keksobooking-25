0d4afa07ba66dec9bdc82bc10d330865f6a02d0b
Valery <rozhnova.v@list.ru>
2022-03-01 20:13:52 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-function getRandomIntegerFromRange (min, max) {
+const getRandomIntegerFromRange = (min, max) => {
   if (min < max && min >= 0) {
     return Math.round(Math.random() * (max - min) + min);
   }
   return (`Неверно указан диапазон от ${  min  } до ${  max}`);
-}
-getRandomIntegerFromRange(0, 20);
+};
 
-function getRandomFloatNumber (min, max, digits) {
+const getRandomFloatNumber = (min, max, digits) => {
   if (min < max && min >= 0) {
     const randomNumber = Math.random() * (max - min) + min;
     return randomNumber.toFixed(digits);
   }
   return (`Неверно указан диапазон от ${  min  } до ${  max}`);
-}
+};
 
 // возвращает рандомный элемент массива
-function getRandomArrayElement(array) {
-  return array[getRandomIntegerFromRange(0, array.length - 1)];
-}
+const getRandomArrayElement = (array) => array[getRandomIntegerFromRange(0, array.length - 1)];
 
 // возвращает массив случайной длинны из неповторяющихся значений
-function getRandomArray(array) {
+const getRandomArray = (array) => {
 
   const copyArray = array.slice();
   const newArray = [];
@@ -30,18 +27,40 @@ function getRandomArray(array) {
   }
 
   return newArray;
-}
+};
 
-const title = [
+const SIMILAR_OFFER_COUNT = 10;
+
+const Location = {
+  DIGIT: 5,
+  MIN_LNG: 139.70000,
+  MAX_LNG: 139.80000,
+  MIN_LAT: 35.65000,
+  MAX_LAT: 35.70000,
+};
+
+const Price =  {
+  MIN: 1,
+  MAX: 100000,
+};
+const Room = {
+  MIN: 1,
+  MAX: 15,
+};
+const Guest = {
+  MIN: 1,
+  MAX: 10,
+};
+const TITLES = [
   'Название 1',
   'Название 2',
   'Название 3',
 ];
-const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_VARIANTS = ['12:00', '13:00', '14:00'];
+const CHECKOUT_VARIANTS = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi','dishwasher','parking','washer','elevator','conditioner'];
-const description = [
+const DESCRIPTIONS = [
   'Описание 1',
   'Описание 2',
   'Описание 3',
@@ -51,27 +70,27 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const SIMILAR_OFFER_COUNT = 10;
+
 
 const createOffer = (value, index) => {
-  const lat = getRandomFloatNumber(35.65000, 35.70000, 5);
-  const lng = getRandomFloatNumber(139.70000, 139.80000, 5);
+  const lat = getRandomFloatNumber(Location.MIN_LAT, Location.MAX_LAT, Location.DIGIT);
+  const lng = getRandomFloatNumber(Location.MIN_LNG, Location.MAX_LNG, Location.DIGIT);
 
   return {
     author: {
-      avatar: `img/avatars/user${  String(index+1).padStart(2, '0')  }.png`,
+      avatar: `img/avatars/user${String(index + 1).padStart(2, '0')}.png`,
     },
     offer: {
-      title: getRandomArrayElement(title),
-      address: `${lat  }, ${  lng}`,
-      price: getRandomIntegerFromRange(1, 100000),
-      type: getRandomArrayElement(TYPE),
-      rooms: getRandomIntegerFromRange(1, 15),
-      guests: getRandomIntegerFromRange(1, 10),
-      checkin:  getRandomArrayElement(CHECKIN),
-      checkout: getRandomArrayElement(CHECKOUT),
+      title: getRandomArrayElement(TITLES),
+      address: `${lat}, ${lng}`,
+      price: getRandomIntegerFromRange(Price.MIN, Price.MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomIntegerFromRange(Room.MIN, Room.MAX),
+      guests: getRandomIntegerFromRange(Guest.MIN, Guest.MAX),
+      checkin:  getRandomArrayElement(CHECKIN_VARIANTS),
+      checkout: getRandomArrayElement(CHECKOUT_VARIANTS),
       features: getRandomArray(FEATURES),
-      description: getRandomArrayElement(description),
+      description: getRandomArrayElement(DESCRIPTIONS),
       photos: getRandomArray(PHOTOS),
       location: {
         lat: lat,
@@ -81,8 +100,8 @@ const createOffer = (value, index) => {
   };
 };
 
-const similarOffer = Array.from({length: SIMILAR_OFFER_COUNT}, createOffer);
+const similarOffers = Array.from({length: SIMILAR_OFFER_COUNT}, createOffer);
 
 //Временное решение, для задействовать переменную
-const someFunc = () => similarOffer;
+const someFunc = () => similarOffers;
 someFunc();
